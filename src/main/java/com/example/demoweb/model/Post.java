a5d9d6d2fe0376e0f14520fb99902dff4995a460
MusinaAlexandra <gorgonova@bk.ru>
2020-05-07 16:32:29 +0500

    Вёрстка с использованием классов Bootstrap

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,27 +1,27 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
 
     private String text;
     private Integer likes = 0;
+    private Date creationDate;
+
+    public Post(String text, Date creationDate) {
+        this.text = text;
+        this.creationDate = creationDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
 
     public String getText() {
         return text;
     }
 
-    public void setText(String text) {
-        this.text = text;
-    }
-
     public Integer getLikes() {
         return likes;
     }
-
-    public void setLikes(Integer likes) {
-        this.likes = likes;
-    }
-
-
-
-
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -12,9 +13,7 @@ public class PostService {
     public List<Post> listAllPosts() {
         List<Post> list = new ArrayList<Post>();
         for (int i = 1; i<= 3; i++) {
-            Post post = new Post();
-            post.setText("Text " + i);
-            post.setLikes(i);
+            Post post = new Post("Text " + i, new Date());
             list.add(post);
         }
         return list;
